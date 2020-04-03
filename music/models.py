f426bb9343bf13f04792d31202babc6f82076e39
ar-munna <armunna1997@gmail.com>
2020-04-04 02:20:46 +0600

    commit 2

--- music/models.py
+++ music/models.py
@@ -8,3 +8,12 @@ class Album(models.Model):
     album_name = models.CharField(max_length=500)
     genre = models.CharField(max_length=250)
     album_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.album_name + '-' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
